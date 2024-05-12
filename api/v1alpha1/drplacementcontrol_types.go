e3ab867d55b82d791ecb3b50ed1417951ee13e25
Raghavendra Talur <raghavendra.talur@gmail.com>
2024-05-12 09:52:53 -0400

    api: add new progression for waiting on user cleanup
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>
    (cherry picked from commit 8f6974a86630a7e9f7931c9b5a823558d3c9274e)

--- api/v1alpha1/drplacementcontrol_types.go
+++ api/v1alpha1/drplacementcontrol_types.go
@@ -102,6 +102,7 @@ const (
 	ProgressionUpdatingPlRule                      = ProgressionStatus("UpdatingPlRule")
 	ProgressionWaitForReadiness                    = ProgressionStatus("WaitForReadiness")
 	ProgressionCleaningUp                          = ProgressionStatus("Cleaning Up")
+	ProgressionWaitOnUserToCleanUp                 = ProgressionStatus("WaitOnUserToCleanUp")
 	ProgressionCheckingFailoverPrequisites         = ProgressionStatus("CheckingFailoverPrequisites")
 	ProgressionFailingOverToCluster                = ProgressionStatus("FailingOverToCluster")
 	ProgressionWaitForFencing                      = ProgressionStatus("WaitForFencing")
